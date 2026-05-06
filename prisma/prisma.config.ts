e63f3b0f0b562ce727acf8ada3a3c956794ef973
jaynikam2005 <jaynikam2005@gmail.com>
2026-05-06 12:25:31 +0530

    Update prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,7 +2,4 @@ import path from "path"
 
 export default {
     schema: path.join("prisma", "schema.prisma"),
-    datasource: {
-        url: process.env.DATABASE_MIGRATE_URL || process.env.DATABASE_URL,
-    },
 }
